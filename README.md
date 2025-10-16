157ca12451be0cd034951bca8daabe03430caae4
Lucio <ulcio.raimondi@protonmail.com>
2025-10-16 16:47:13 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,6 @@
+#Instrucciones de uso
+1. Cargar el proyecto codigo_stm32 en STM32CubeIDE
+2. Compilar el proyecto
+3. Cargar el proyecto en el microcontrolador
+4. Colocar el auto sobre la pista
+5. Encender las baterias
---
+++ codigo_stm32/Debug/Core/Src/Statechart.d
@@ -0,0 +1,8 @@
+Core/Src/Statechart.o: ../Core/Src/Statechart.c ../Core/Inc/sc_types.h \
+ ../Core/Inc/Statechart.h ../Core/Inc/sc_types.h \
+ ../Core/Inc/Statechart_required.h ../Core/Inc/Statechart.h
+../Core/Inc/sc_types.h:
+../Core/Inc/Statechart.h:
+../Core/Inc/sc_types.h:
+../Core/Inc/Statechart_required.h:
+../Core/Inc/Statechart.h:
